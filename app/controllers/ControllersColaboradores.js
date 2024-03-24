0e435448c1715b6c574c52a8958395106eb84830
Carlos <carlos.admcode@gmail.com>
2024-03-24 12:06:30 -0300

    feat: Gerando pdf apartir de uma arquivo html

--- app/controllers/ControllersColaboradores.js
+++ app/controllers/ControllersColaboradores.js
@@ -1,9 +1,12 @@
 const Colaboradores = require("../models/Colaboradores");
 const Beneficios = require("../models/Beneficios");
-const fs = require('fs');
+const path = require("path");
+const ejs =require("ejs");
+const pdf = require("html-pdf")
 
 class ControllersColaboradores{
-    constructor(){
+    constructor()
+    {
         this.colaboradores = new Colaboradores();
         this.beneficios = new Beneficios();
     };
@@ -11,14 +14,16 @@ class ControllersColaboradores{
     // Renderiza uma lista dos colaboradores relacionados a empresa:
     renderTableColaboradores(req, res){
         this.colaboradores.read(
-            (error, result) => {
+            (error, result) =>
+            {
                 // Confirmo se a requisição ao banco deu certo:(SE sim)
-                if(!error){
+                if(!error)
+                {
                     res.render("pages_admin/table_colaboradores",{colaboradores:result, id_empresa: req.params.id});
                 }
                 //(Se não)
-                else{
-                    console.log(error);
+                else {
+                    return res.send(error);
                 };
             },
             req.params.id
@@ -26,12 +31,14 @@ class ControllersColaboradores{
     };
 
     // Renderiza o formulario de cadastro de colaboradores:
-    renderFormColaboradores(req, res){
+    renderFormColaboradores(req, res)
+    {
         try {
             res.render("pages_admin/form_colaboradores", {action:`/colaboradores/new/${req.params.id}`, colaborador:[]});
         }
-        catch (error) {
-            console.log(error);
+        catch (error)
+        {
+            return res.send(error);
         }
 
         /*
@@ -41,22 +48,24 @@ class ControllersColaboradores{
                 res.render("pages_admin/form_colaboradores", {id:req.params.id, beneficios:result});
             }
             else{
-                console.log(error);
+                return res.send(error);
             }
         });
         */
     };
 
     // Renderiza o formulario de editar colaboradores:
-    renderFormEditColaboradores(req, res){
-        this.colaboradores.read(
-            (error, result) => {
+    renderFormEditColaboradores(req, res)
+    {
+        this.colaboradores.read((error, result) =>
+            {
                 // Confirma se a query foi executada corretamente:(Se sim)
-                if(!error){
+                if(!error)
+                {
                     res.render("pages_admin/form_colaboradores", {action:`/colaboradores/edit/${req.params.id}`, colaborador:result});
                 }
                 else{
-                    console.log(error);
+                    return res.send(error);
                 }
             },
             0,
@@ -65,7 +74,8 @@ class ControllersColaboradores{
     };
 
     //Cria um novo colaborador e renderiza a nova lista de colaboradores:
-    createColaboradores(req, res){
+    createColaboradores(req, res)
+    {
         this.colaboradores = new Colaboradores(
             0,
             req.body.nome,
@@ -82,20 +92,22 @@ class ControllersColaboradores{
             req.body.FK_periodo,
             req.body.FK_turno
         );
-        this.colaboradores.create((error, result) => {
+        this.colaboradores.create((error, result) => 
+        {
             // Comfirma se a query foi executada corretamente:(Se sim)
             if(!error){
                 res.redirect('/colaboradores/list/' + req.params.id);
             }
             //(Se não)
             else{
-                console.log(error);
+                return res.send(error);
             };
         });
     };
 
     //Edita o cadastro de um colaborador e renderiza a nova lista de colaboradores:
-    updateColaboradores(req, res){
+    updateColaboradores(req, res)
+    {
         this.colaboradores = new Colaboradores(
             req.body.ID_login,
             req.body.nome,
@@ -112,20 +124,22 @@ class ControllersColaboradores{
             req.body.FK_periodo,
             req.body.FK_turno
         );
-        this.colaboradores.update((error, result) => {
+        this.colaboradores.update((error, result) => 
+        {
             // Comfirma se a query foi executada corretamente:(Se sim)
             if(!error){
                 res.redirect('/colaboradores/list/' + req.body.FK_empresa);
             }
             //(Se não)
             else{
-                console.log(error);
+                return res.send(error);
             };
         });
     };
 
     // Apaga um cadastro de colaborador:
-    deleteColaboradores(req, res){
+    deleteColaboradores(req, res)
+    {
         this.colaboradores.delete(req.params.id, (error, result) => {
             // Comfirma se a query foi executada corretamente:(Se sim)
             if(!error){
@@ -133,87 +147,78 @@ class ControllersColaboradores{
             }
             //(Se não)
             else{
-                console.log(error);
+                return res.send(error);
             };
         });
     };
 
-    printColaboradores(req, res){
-        /*
-        const pdfmake = require('pdfmake');
-        const printer = new pdfmake({
-        Roboto: {
-            normal: './app/public/fonts/Roboto_Slab/static/RobotoSlab-Regular.ttf',
-            bold: './app/public/fonts/Roboto_Slab/static/RobotoSlab-Medium.ttf',
-            italics: './app/public/fonts/Roboto_Slab/static/RobotoSlab-Italic.ttf',
-            bolditalics: './app/public/fonts/Roboto_Slab/static/RobotoSlab-MediumItalic.ttf'
-        }
-        });
-
-        let funcionarios = [
-        { nome: 'Funcionario 1', cargo: 'Cargo 1' },
-        { nome: 'Funcionario 2', cargo: 'Cargo 2' },
-        // adicione mais funcionários conforme necessário
-        ];
-
-        let corpo = funcionarios.map(f => [f.nome, f.cargo]);
-
-        let docDefinition = {
-        content: [
-            { text: 'Lista de Funcionários', style: 'header' },
+    printColaboradores(req, res)
+    {
+        const filepath =  path.join(path.resolve(__dirname, '..'), "./views/components", "print.ejs");
+        // Chama função que le a tabela de  colaboradores:
+        this.colaboradores.read((error, result) => {
+            // Verifica se a querry foi executada corretamente:(Se sim)
+            if(!error)
             {
-            table: {
-                body: [['Nome', 'Cargo'], ...corpo]
-            }
-            }
-        ],
-        styles: {
-            header: {
-            fontSize: 18,
-            bold: true,
-            margin: [0, 0, 0, 10]
-            }
-        }
-        };
+                ejs.renderFile(
+                    filepath,
+                    {colaboradores: result},
+                    (error, result) => {
+                        if(error)
+                        {
+                            return res.send('Erro na leitura do arquivo');
+                        }
+                        // Cofiguração de pdf folha A4:
+                        const options = {
+                            height: "11.25in",
+                            width: "8.5in",
+                            header:{
+                                height: "20mm"
+                            },
+                            footer: {
+                                height: "20mm"
+                            }
+                        }
 
-        let pdfDoc = printer.createPdfKitDocument(docDefinition);
-        pdfDoc.pipe(fs.createWriteStream('Lista_de_Funcionarios.pdf'));
-        res.redirect('Lista_de_Funcionarios.pdf');
-        pdfDoc.end();
-        */
+                        // Criar o pdf:
+                        pdf.create(result, options).toFile("report.pdf", (error, data) =>
+                        {
+                            if(error)
+                            {
+                                return res.send('Erro ao criar pdf');
+                            }
+                            // Enviar para o navegador:
+                            return res.send(result);
+                        });
+                    }
+                );
+            } 
+        }, req.params.id);
+    }
 
-        (async () => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-        
-            // Defina seu HTML aqui. Eu estou usando uma tabela simples para este exemplo.
-            const content = `
-                <html>
-                <body>
-                    <table>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Telefone</th>
-                        <th>Cargo</th>
-                    </tr>
-                    <tr>
-                        <td>João Silva</td>
-                        <td>joao.silva@empresa.com.br</td>
-                        <td>(31) 98765-4321</td>
-                        <td>Gerente</td>
-                    </tr>
-                    <!-- Adicione mais colaboradores conforme necessário -->
-                    </table>
-                </body>
-                </html>
-            `;
-        
-            await page.setContent(content);
-            await page.pdf({ path: 'lista_de_colaboradores.pdf', format: 'A4' });
-        
-            //await browser.close();
-        })();
+    printStyleColaboradores(req, res)
+    {
+        const filepath =  path.join(path.resolve(__dirname, '..'), "./views/components", "print.ejs");
+        // Chama função que le a tabela de  colaboradores:
+        this.colaboradores.read((error, result) => {
+            // Verifica se a querry foi executada corretamente:(Se sim)
+            if(!error)
+            {
+                ejs.renderFile(
+                    filepath,
+                    {colaboradores: result},
+                    (error, result) => {
+                        if(error)
+                        {
+                            return res.send('Erro na leitura do arquivo');
+                        }
+                        
+                        // Enviar para o navegador:
+                        return res.send(result);
+                    }
+                );
+            } 
+        }, req.params.id);
     }
 };
 
--- app/routes/Colaboradores.js
+++ app/routes/Colaboradores.js
@@ -1,5 +1,5 @@
 const ControllersColaboradores = require("../controllers/ControllersColaboradores");
-
+const puppeteer = require("puppeteer")
 module.exports = (app) => {
     app.get('/colaboradores/list/:id', (req, res) => {
         ControllersColaboradores.renderTableColaboradores(req, res);
@@ -27,8 +27,35 @@ module.exports = (app) => {
 
     app.get('/colaboradores/horas', (req, res) => {
         ControllersColaboradores.renderFormHoras(req, res);
-    })
-    app.get('/colaboradores/print', (req, res) => {
+    });
+    app.get('/colaboradores/print/:id', (req, res) => {
         ControllersColaboradores.printColaboradores(req, res);
-    })
+    });
+    app.get('/colaboradores/style/print/:id', async (req, res) => {
+        // Abrindo uma nova pagina do navegador:
+        const browser = await puppeteer.launch({headless:false});
+        const page = await browser.newPage();
+
+        await page.goto('http://127.0.0.1:4755/colaboradores/print/'+ req.params.id, {
+            waitUntil: 'networkidle0'
+        });
+
+        // Configurando estilo do pdf:
+        const pdf = await page.pdf({
+            printBackground: true,
+            format: 'Letter',
+            margin: {
+                top: "20px",
+                bottom: "40px",
+                left: "20px",
+                right: "20px"
+            }
+        });
+
+        await browser.close();
+
+        res.contentType("application/pdf");
+        return res.send(pdf);
+        //ControllersColaboradores.printStyleColaboradores(req, res);
+    });
 };
